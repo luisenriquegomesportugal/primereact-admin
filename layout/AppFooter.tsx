2b544b46d71649745c0813cc2f26fb9c56d0808b
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-06-20 12:08:26 -0300

    first commit

---
+++ layout/AppFooter.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { useContext } from 'react';
+import { LayoutContext } from './context/layoutcontext';
+
+const AppFooter = () => {
+    const { layoutConfig } = useContext(LayoutContext);
+    return <div className="layout-footer"></div>;
+};  
+
+export default AppFooter;
